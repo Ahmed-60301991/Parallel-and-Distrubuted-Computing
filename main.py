adb00030ff20f8641d5192445ae0adc633b569fa
Ahmed ADAMU <60301991@udst.edu.qa>
2025-03-02 12:16:20 +0300

    Distributed computing lecture 1

--- main.py
+++ main.py
@@ -1,39 +1,10 @@
-import threading
-import time
-from src.sensor import simulate_sensor, initialize_sensors
-from src.processing import process_temperatures
-from src.display import initialize_display, update_display
-
-def main():
-    num_sensors = 7  # Number of sensors
-
-    # Initialize sensors
-    initialize_sensors(num_sensors)
-
-    # Initialize display
-    initialize_display(num_sensors)
-
-    # Create and start sensor threads
-    sensor_threads = []
-    for sensor_id in range(1, num_sensors + 1):
-        thread = threading.Thread(target=simulate_sensor, args=(sensor_id,), daemon=True)
-        sensor_threads.append(thread)
-        thread.start()
-
-    # Create and start the processing thread
-    process_thread = threading.Thread(target=process_temperatures, daemon=True)
-    process_thread.start()
-
-    # Start the display update thread
-    display_thread = threading.Thread(target=update_display, args=(num_sensors,), daemon=True)
-    display_thread.start()
-
-    # Keep the main thread alive to allow daemon threads to run
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("\nStopping simulation...")
-
-if __name__ == "__main__":
-    main()
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+if rank == 0:
+    data = {"a": 7, "b": 3.14}
+    print("On process 0, sending this data:", data)
+    comm.send(data, dest=1, tag=11)
+elif rank == 1:
+    data = comm.recv(source=0, tag=11)
+    print("On process 1, data received:", data)
